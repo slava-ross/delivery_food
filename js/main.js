e598b13829af6ff22f77ecc27a1c02270c7c58fc
Yaroslav Yagodarov <slava-ross@yandex.ru>
2020-04-10 22:33:34 +0400

    add cancel button click-action

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const cartButton = document.querySelector('#cart-button');
+const cancel = document.querySelector('#cancel');
+
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
 
 cartButton.addEventListener('click', toggleModal);
 close.addEventListener('click', toggleModal);
+cancel.addEventListener('click', toggleModal);
 
 function toggleModal() {
     modal.classList.toggle('is-open');
